e76d612edd7a14ac16aec094683a39612e9ad238
els-lartee <166175752+els-lartee@users.noreply.github.com>
2024-04-10 20:26:11 +0000

    elsie

---
+++ EventManagement.java
@@ -0,0 +1,28 @@
+public class EventManagement{
+    private boolean  isEvent;
+    
+    //constructor
+    EventManagement(boolean isEvent){
+        this.isEvent= isEvent;
+
+    }
+
+    //getters
+    public boolean getisEvent(){
+        return getisEvent;
+    }
+
+    //setters
+    public boolean setisEvent(){
+        this.isEvent= isEvent;
+
+    }
+
+    public scheduleEvent( Date eventDate, String parkingZone){
+
+    }
+
+    public cancelEvent(int eventID)
+
+
+}
