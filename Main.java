d4a93503408544f2557fd69ae877c39868606ce7
Nana Amoako <63524504+nanadotam@users.noreply.github.com>
2024-04-10 21:13:14 +0000

    added main class

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
-public class Main{
-    public static main void(String []args){
-        ParkingPermit p = new ParkingPermit("GW122", "73482028", LocalDate validFrom, LocalDate validUntil, String permitType)
+// public class Main{
+//     public static main void(String []args){
+//         ParkingPermit p = new ParkingPermit("GW122", "73482028", LocalDate validFrom, LocalDate validUntil, String permitType)
+//     }
+// }
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Example of creating and using a Vehicle
+        Vehicle vehicle = new Vehicle("ABC123", false);
+        System.out.println("Vehicle License Plate: " + vehicle.getLicensePlate());
+
+        // Example of creating a User
+        User user = new User("userID123", "John Doe", "staff");
+        System.out.println("User Role: " + user.getRole());
+
+        // Example of managing a parking spot
+        ParkingSpot spot = new ParkingSpot("spotID1", "near entrance", false, "general");
+        System.out.println("Parking Spot Type: " + spot.getType());
+
+        // Assume more complex interactions and integrations here
     }
 }
